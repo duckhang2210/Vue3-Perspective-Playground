57d8b8546bab3df4661f04b27c05374097834fc7
unknown <duckhang2210@gmail.com>
2021-10-12 14:29:03 -0500

    add some box, border customizations

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ Vue.createApp({
 			perspective: 100,
 			rotateX: 0,
 			rotateY: 0,
-			rotateZ: 0
+			rotateZ: 0,
+			boxColor: '',
+			borderColor: '',
+			borderRadius: 0,
+			boxRadius: 0
 		};
 	},
 	computed: {
@@ -15,7 +19,15 @@ Vue.createApp({
                     rotateX(${this.rotateX}deg)
                     rotateY(${this.rotateY}deg)
                     rotateZ(${this.rotateZ}deg)
-                `
+                `,
+				background: this.boxColor,
+				'border-radius': `${this.boxRadius}px`
+			};
+		},
+		boxContainer() {
+			return {
+				'border-color': this.borderColor,
+				'border-radius': `${this.borderRadius}px`
 			};
 		}
 	},
@@ -25,6 +37,10 @@ Vue.createApp({
 			this.rotateX = 0;
 			this.rotateY = 0;
 			this.rotateZ = 0;
+			this.boxColor = '';
+			this.borderColor = '';
+			this.borderRadius = 0;
+			this.boxRadius = 0;
 		}
 	}
 }).mount('#app');
